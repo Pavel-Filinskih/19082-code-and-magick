a4ecb6b4f813421d6d1f6d1f1746b0263a7bc58c
Pavel-Filinskih <pavfilin@gmail.com>
2017-02-04 14:23:57 +0500

    фикс index.html и setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,35 +4,11 @@
 var popupWindow = document.querySelector('.setup-open');
 var openPopupShow = document.querySelector('.setup');
 
-popupWindow.addEventListener('click', function () {
-  openPopupShow.classList.remove('invisible');
-});
-
-// закрываем popup окно кликнув по крестику X
-var closePopupWindow = document.querySelector('.setup-close');
-
-closePopupWindow.addEventListener('click', function () {
-  openPopupShow.classList.add('invisible');
-});
-
+var saveUserConfig = document.querySelector('.setup-submit');
 // валидация имени персонажа
 var userNameText = document.querySelector('.setup-user-name');
-
-userNameText.addEventListener('click', function () {
-  userNameText.value = '';
-  userNameText.placeholder = 'Введите имя мага';
-});
-
-var saveUserConfig = document.querySelector('.setup-submit');
-
-saveUserConfig.addEventListener('click', function () {
-  userNameText.required = true;
-
-  if (userNameText.value.length > 50) {
-    userNameText.value = '';
-    userNameText.placeholder = 'Max 50 символов!';
-  }
-});
+// закрываем popup окно кликнув по крестику X
+var closePopupWindow = document.querySelector('.setup-close');
 
 // раскраска мантии у мага
 var colorMantMag = document.getElementById('wizard-coat');
@@ -44,21 +20,6 @@ var colorRgbMantle = [
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'];
 
-colorMantMag.addEventListener('click', function () {
-
-  function colorRgbRandom(arr, obj) {
-    for (var i = 0; i < 10; i++) {
-
-      var randRgbColor = arr[Math.floor(Math.random() * arr.length)];
-      if (obj.style.fill !== randRgbColor) {
-        obj.style.fill = randRgbColor;
-        break;
-      }
-    }
-  }
-  colorRgbRandom(colorRgbMantle, colorMantMag);
-});
-
 // смена цвета глаз у мага
 var colorEyesMag = document.getElementById('wizard-eyes');
 var colorKeyEyes = [
@@ -67,19 +28,6 @@ var colorKeyEyes = [
   'blue',
   'yellow',
   'green'];
-
-colorEyesMag.addEventListener('click', function () {
-
-  for (var i = 0; i < 10; i++) {
-
-    var randKeyColor = colorKeyEyes[Math.floor(Math.random() * colorKeyEyes.length)];
-
-    if (colorEyesMag.style.fill !== randKeyColor) {
-      colorEyesMag.style.fill = randKeyColor;
-      break;
-    }
-  }
-});
 
 // смена цвета файерболла мага
 var colorFireballMage = document.querySelector('.setup-fireball-wrap');
@@ -90,18 +38,43 @@ var colorHexFireball = [
   '#e848d5',
   '#e6e848'];
 
-colorFireballMage.style.background = '#ee4830';
-var colorIndex = 1;
+// произвольный цвет из массива
+var randomColor = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// установка произвольного цвета в элемент
+var colorRgbRandom = function (arr, obj, props) {
+  var randRgbColor = randomColor(arr);
+
+  obj.style[props] = randRgbColor;
+};
+
+popupWindow.addEventListener('click', function () {
+  openPopupShow.classList.remove('invisible');
+});
+
+closePopupWindow.addEventListener('click', function () {
+  openPopupShow.classList.add('invisible');
+});
+
+userNameText.addEventListener('click', function () {
+  userNameText.value = '';
+  userNameText.placeholder = 'Введите имя мага';
+});
+
+saveUserConfig.addEventListener('click', function () {
+  userNameText.required = true;
+});
+
+colorMantMag.addEventListener('click', function () {
+  colorRgbRandom(colorRgbMantle, colorMantMag, 'fill');
+});
+
+colorEyesMag.addEventListener('click', function () {
+  colorRgbRandom(colorKeyEyes, colorEyesMag, 'fill');
+});
 
 colorFireballMage.addEventListener('click', function () {
-
-  if (colorIndex < colorHexFireball.length) {
-    if (colorIndex < colorHexFireball.length) {
-      colorFireballMage.style.backgroundColor = colorHexFireball[colorIndex];
-    }
-  }
-  colorIndex++;
-  if (colorIndex === colorHexFireball.length) {
-    colorIndex = 0;
-  }
+  colorRgbRandom(colorHexFireball, colorFireballMage, 'backgroundColor');
 });
